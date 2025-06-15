bc6d3b449a11cdcab326fccf1c26db4197d76e16
sumit kumar <sumit142929@gmail.com>
2025-06-15 22:45:03 +0530

    Add files via upload

---
+++ Image Gallery/script.js
@@ -0,0 +1,25 @@
+let list=document.querySelectorAll('.list');
+let itembox=document.querySelectorAll('.itembox');
+
+for(let i=0; i<list.length; i++){
+list[i].addEventListener('click',function(){
+    for(let j=0; j<list.length; j++){
+        list[j].classList.remove('active');
+    }
+    this.classList.add('active');
+
+    let datafilter= this.getAttribute('data-filter');
+
+    for(let k=0; k<itembox.length; k++){
+        itembox[k].classList.remove('hide');
+         itembox[k].classList.add('hide');
+
+        if(itembox[k].getAttribute('data-item')== datafilter || 
+    datafilter =="all"){
+        itembox[k].classList.remove('hide');
+        itembox[k].classList.add('active');
+
+    }
+    }
+})
+}
